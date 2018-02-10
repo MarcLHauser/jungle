c96ab8e732963151975461b46b6624444798d4d1
Marc H <mhauser@ocean7.com>
2018-02-09 23:17:29 -0500

    link 2 objects

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ lithuania = {
     question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
 };
 
+america = {
+    question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?"
+};
+
+lithuania.yes = america;
+
 var place = lithuania;
 
 var ask = function() {
