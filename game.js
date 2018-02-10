0f42d0a3024798e41d6d80e863df41c1c0fe3d47
gnumskull <gnumskull@marchauser.org>
2018-02-09 22:35:37 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+lithuania = {
+    question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+};
+
+var ask = function() {
+    return lithuania.question;
+};
+
+console.log( ask() );
