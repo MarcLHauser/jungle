41604c8b06e7c2a3e09644bfc34c170be2ef3224
Marc <nunya@biz-ness>
2018-02-10 15:17:29 -0500

    create death object

--- game.js
+++ game.js
@@ -1,25 +1,27 @@
-lithuania = {
-    question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+var data = {
+    lithuania : {
+        question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+    },
+    america : {
+        question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?"
+    },
+    freeze : {
+        death: "You have frozen to death because of Lithuania's cold climate."
+    }
 };
 
-america = {
-    question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?"
-};
+data.lithuania.yes = data.america;
 
-freeze = {
-    death: "You have frozen to death because of Lithuania's cold climate."
-};
+var place = data.lithuania;
 
-lithuania.yes = america;
 
-var place = lithuania;
 
 var ask = function() {
     return place.question;
 };
 
 var yes = function(){
-	place = place.yes;
+    place = place.yes;
     return ask();
 };
 
