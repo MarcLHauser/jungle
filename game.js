321d20510547c701d5ec30e955d9fa9093924eeb
Marc <nunya@biz-ness>
2018-02-10 16:37:19 -0500

    link objects

--- game.js
+++ game.js
@@ -9,18 +9,18 @@ var data = {
 
 data.lithuania.yes = data.america;
 
-var place = data.lithuania;
-
-var ask = function() {
-    return place.question;
+var getMessage = function() {
+    return data.lithuania.question;
 };
 
 var yes = function(){
-    return ask();
+    return getMessage();
 };
 
 var no = function(){
-    return ask();
+    return getMessage();
 };
 
-console.log( ask() );
+// start game
+var place = data.lithuania;
+console.log( getMessage() );
