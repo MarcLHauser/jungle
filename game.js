c21b910c3ba635089f38d9ff51d3716d38b519da
Marc <nunya@biz-ness>
2018-02-10 14:42:00 -0500

    functions: yes,no

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
-lithuania = {
-    question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+var data = {
+    lithuania : {
+        question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+    }
 };
 
 var ask = function() {
-    return lithuania.question;
+    return data.lithuania.question;
 };
 
 var yes = function(){
