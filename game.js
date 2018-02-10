8c9d6bb7e1e24389d5bca8bf9e43dd6c5caff36e
Marc H <mhauser@ocean7.com>
2018-02-10 00:00:14 -0500

    add data

--- game.js
+++ game.js
@@ -10,8 +10,58 @@ freeze = {
     death: "You have frozen to death because of Lithuania's cold climate."
 };
 
+alleyway = {
+    death: "You decide to sleep in an abandoned alleyway and find all your money gone in the morning. You starve to death because no one will give you a job."
+};
+
+hotel = {
+    question: "The man makes you pay enormous fees to get out. Once you get out of the hotel, you remember that your friend got rich in Chicago. Do you want to go to Chicago?"
+};
+
+newyork = {
+    question: "You stay in New York. Do you want to stay in a tenement?"
+};
+
+tuberculosis = {
+    death: "You get tuberculosis after a few days in the tenement. It is fatal."
+};
+
+homeless = {
+    death: "You are homeless and you can't find work. You starve."
+};
+
+chicago = {
+    question: "You finally arrive in Chicago and you learn a new word: 'stockyards'. Do you want to go to the stockyards?"
+};
+
+wandering = {
+    death: "You starve wandering Chicago."
+};
+
+stockyards = {
+    question: "You arrive in the stockyards. Coincidencetally, you run into your friend who owns a bodega. He gives a recommendation for a boardinghouse. Do you stay there?"
+};
+
+streets = {
+    death:  "You quickly realize that the whole family cannot sleep in the streets. You find a tenement building to stay in for very little money, but it is very small and cramped. You get sick, because of the cramped consitions, and the disease is fatal."
+};
+
+boardinghouse = {
+    question: 'More questions to come...'
+};
+
 lithuania.yes 	= america;
 lithuania.no 	= freeze;
+america.yes     = hotel;
+america.no      = alleyway;
+hotel.yes       = chicago;
+hotel.no        = newyork;
+newyork.yes     = tuberculosis;
+newyork.no      = homeless;
+chicago.yes     = stockyards;
+chicago.no      = wandering;
+stockyards.yes  = boardinghouse;
+stockyards.no   = streets;
 
 var place = lithuania;
 
