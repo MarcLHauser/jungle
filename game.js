621e5925e59e34cf69deb5edfbcd4107d480cb7c
Marc <nunya@biz-ness>
2018-02-10 15:32:50 -0500

    show question, cause of death

--- game.js
+++ game.js
@@ -1,19 +1,19 @@
-lithuania = {
-    question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+var data = {
+    lithuania : {
+        question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
+    },
+    america : {
+        question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?"
+    },
+    freeze : {
+        death: "You have frozen to death because of Lithuania's cold climate."
+    }
 };
 
-america = {
-    question: "You arrive in New York. A man in a blue uniform says he knows a place you can stay at. Do you follow him to a hotel?"
-};
+data.lithuania.yes = data.america;
+data.lithuania.no = data.freeze;
 
-freeze = {
-    death: "You have frozen to death because of Lithuania's cold climate."
-};
-
-lithuania.yes 	= america;
-lithuania.no 	= freeze;
-
-var place = lithuania;
+var place = data.lithuania;
 
 var ask = function() {
 	if (place.question) {
