0ab158bfc00742ee92ddc999c4d7700ddcd45b64
Marc H <mhauser@ocean7.com>
2018-02-10 21:17:41 -0500

    wrap program in IIFE

--- game.js
+++ game.js
@@ -2,8 +2,7 @@
 
     var data = {
         lithuania : {
-            question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'",
-            yes: "america"
+            question: "You are Jurgis, a 27-year-old Lithuanian man who has fallen in love with Ona, a 16-year-old girl. Do you want to go to America with her and her family? Answer 'yes()' or 'no()'"
         },
 
         america : {
